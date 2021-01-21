8e50b16655c359ec2cbbda4e4dce66ef996f4763
Noah-43 <ajph03102@naver.com>
2021-01-22 02:58:38 +0900

    업로드 이미지 출력 #3

--- src/main/java/nhn/rookie/hama/mreview/controller/UploadController.java
+++ src/main/java/nhn/rookie/hama/mreview/controller/UploadController.java
@@ -3,14 +3,19 @@ package nhn.rookie.hama.mreview.controller;
 import lombok.extern.log4j.Log4j2;
 import nhn.rookie.hama.mreview.dto.UploadResultDTO;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
@@ -84,5 +89,35 @@ public class UploadController {
 
         return folderPath;
     }
+
+    // 인코딩된 파일 이름을 파라미터로 받아서 해당 파일은 byte[]로 만들어서 브라우저로 전송
+    @GetMapping("/display")
+    public ResponseEntity<byte[]> getFile(String fileName) {
+
+        ResponseEntity<byte[]> result = null;
+
+        try {
+            String srcFileName = URLDecoder.decode(fileName, "UTF-8");
+
+            log.info("fileName: " + srcFileName);
+
+            File file = new File(uploadPath+File.separator+srcFileName);
+
+            log.info("file: "+file);
+
+            HttpHeaders header = new HttpHeaders();
+
+            // MIME 타입 처리
+            header.add("Content-Type", Files.probeContentType(file.toPath()));
+
+            // 파일 데이터 처리
+            result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return result;
+    }
+
 
 }

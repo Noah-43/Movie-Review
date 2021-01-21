ee07568b32f9a40381e02a7d767b8058f65f2fa8
Noah-43 <ajph03102@naver.com>
2021-01-22 00:33:56 +0900

    데이터 출력을 위한 쿼리 작성 #2

--- src/main/java/nhn/rookie/hama/mreview/repository/MovieRepository.java
+++ src/main/java/nhn/rookie/hama/mreview/repository/MovieRepository.java
@@ -1,7 +1,24 @@
 package nhn.rookie.hama.mreview.repository;
 
 import nhn.rookie.hama.mreview.entity.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    // coalesce(expression1, ...n)는 지정한 표현식들 중에 NULL이 아닌 첫 번깨 값을 반환
+    @Query("select m, mi, avg(coalesce(r.grade,0)), count(distinct r) from Movie m " +
+            "left outer join MovieImage mi on mi.movie = m " +
+            "left outer join Review r on r.movie = m group by m")
+    Page<Object[]> getListPage(Pageable pageable); // 페이지 처리
+
+    @Query("select m, mi, avg(coalesce(r.grade,0)), count(r) " +
+            " from Movie m left outer join MovieImage mi on mi.movie = m " +
+            " left outer join Review r on r.movie = m " +
+            "where m.mno = :mno group by mi")
+    List<Object[]> getMovieWithAll(Long mno); // 특정 영화 조회
 }
--- src/main/java/nhn/rookie/hama/mreview/repository/ReviewRepository.java
+++ src/main/java/nhn/rookie/hama/mreview/repository/ReviewRepository.java
@@ -1,7 +1,19 @@
 package nhn.rookie.hama.mreview.repository;
 
+import nhn.rookie.hama.mreview.entity.Movie;
 import nhn.rookie.hama.mreview.entity.Review;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    // @EntityGraph는 엔티티의 특정한 속성을 같이 로딩하도록 표시하는 어노테이션(특정 기능을 수행할 때만 EAGER 로딩 지정 가능)
+    // attributePaths - 로딩 설정을 변경하고 싶은 속성의 이름을 배열로 명시
+    // type - @EntityGraph를 어떤 방식으로 적용할 것인지 설정
+    // FETCH: 명시한 속성은 EAGER로 처리, 나머지 LAZY 처리.
+    // LOAD: 명시한 속선은 EAGER로 처리, 나머지는 엔티티 클래스에 명시되거나 기본 방식으로 처리.
+    @EntityGraph(attributePaths = {"member"}, type = EntityGraph.EntityGraphType.FETCH)
+    List<Review> findByMovie(Movie movie);
 }

7dfad2fdcd9fe19d6800c58b14f34eb84ac746d1
Noah-43 <ajph03102@naver.com>
2021-01-22 02:42:37 +0900

    업로드 결과 반환 #3
    JSON의 배열 형태로 결과 전달

--- src/main/java/nhn/rookie/hama/mreview/controller/UploadController.java
+++ src/main/java/nhn/rookie/hama/mreview/controller/UploadController.java
@@ -1,7 +1,10 @@
 package nhn.rookie.hama.mreview.controller;
 
 import lombok.extern.log4j.Log4j2;
+import nhn.rookie.hama.mreview.dto.UploadResultDTO;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,6 +15,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -23,14 +28,16 @@ public class UploadController {
 
 
     @PostMapping("/uploadAjax")
-    public void uploadFile(MultipartFile[] uploadFiles) { // 배열을 활용하면 동시에 여러 개의 파일 정보 처리 가능
+    public ResponseEntity<List<UploadResultDTO>> uploadFile(MultipartFile[] uploadFiles) { // 배열을 활용하면 동시에 여러 개의 파일 정보 처리 가능
+
+        List<UploadResultDTO> resultDTOList = new ArrayList<>();
 
         for (MultipartFile uploadFile : uploadFiles) {
 
             // 이미지 파일만 업로드 가능
             if(uploadFile.getContentType().startsWith("image") == false) {
                 log.warn("this file is not image type");
-                return;
+                return new ResponseEntity<>(HttpStatus.FORBIDDEN); // 403
             }
 
             // 실제 파일 이름 IE나 Edge는 전체 경로가 들어오므로
@@ -50,11 +57,16 @@ public class UploadController {
             Path savePath = Paths.get(saveName);
 
             try {
-                uploadFile.transferTo(savePath);
+                uploadFile.transferTo(savePath); // 실제 이미지 저장
+                resultDTOList.add(new UploadResultDTO(fileName, uuid, folderPath));
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
         } // end for
+
+        // 브라우저는 업로드 처리 후 JSON의 배열 형태로 결과를 전달 받음.
+        return new ResponseEntity<>(resultDTOList, HttpStatus.OK);
     }
 
     private String makeFolder() {

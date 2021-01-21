a798a16821ee2a9b993a22dfa53dda94aab2ff30
Noah-43 <ajph03102@naver.com>
2021-01-22 02:29:43 +0900

    업로드 파일 저장 기능 구현 #3

--- src/main/java/nhn/rookie/hama/mreview/controller/UploadController.java
+++ src/main/java/nhn/rookie/hama/mreview/controller/UploadController.java
@@ -1,25 +1,76 @@
 package nhn.rookie.hama.mreview.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
 
 @RestController
 @Log4j2
 public class UploadController {
 
+    @Value("${nhn.rookie.hama.upload.path}") // application.properties의 변수
+    private  String uploadPath;
+
+
     @PostMapping("/uploadAjax")
     public void uploadFile(MultipartFile[] uploadFiles) { // 배열을 활용하면 동시에 여러 개의 파일 정보 처리 가능
 
         for (MultipartFile uploadFile : uploadFiles) {
+
+            // 이미지 파일만 업로드 가능
+            if(uploadFile.getContentType().startsWith("image") == false) {
+                log.warn("this file is not image type");
+                return;
+            }
+
             // 실제 파일 이름 IE나 Edge는 전체 경로가 들어오므로
             String originalName = uploadFile.getOriginalFilename();
             String fileName = originalName.substring(originalName.lastIndexOf("\\") + 1);
 
             log.info("fileName: " + fileName);
+            // 날짜 폴더 생성(하나의 폴더에 저장할 수 있는 파일 수 제한 때문에 년/월/일 폴더 따로 생성)
+            String folderPath = makeFolder();
+
+            // UUID
+            String uuid = UUID.randomUUID().toString();
+
+            // 저장할 파일 이름 중간에 "_"를 이용해서 구분
+            String saveName = uploadPath + File.separator + folderPath + File.separator + uuid + "_" + fileName;
+
+            Path savePath = Paths.get(saveName);
+
+            try {
+                uploadFile.transferTo(savePath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } // end for
+    }
+
+    private String makeFolder() {
+
+        String str = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+
+        String folderPath = str.replace("\\", File.separator);
+
+        // make folder ------------
+        File uploadPathFolder = new File(uploadPath, folderPath);
+
+        if(uploadPathFolder.exists() == false) {
+            uploadPathFolder.mkdirs();
         }
 
+        return folderPath;
     }
 
 }
